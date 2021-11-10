5a152a978be3bf489cd30a60041c50902db23b94
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-11-10 16:00:17 +0100

    Fix post model

--- Model/Post.php
+++ Model/Post.php
@@ -3,7 +3,7 @@
 namespace Biglidio\MiniBlog\Model;
 
 use Magento\Framework\Model\AbstractModel;
-use Biglidio\MiniBlog\Model\ResourceModel\Post as PostResourceModel;
+use Biglidio\MiniBlog\Model\ResourceModel\Post as PostResource;
 
 class Post extends AbstractModel
 {
