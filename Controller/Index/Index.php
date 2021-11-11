d66fc7d721634d6002af96badd90221113a5c671
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-11-11 17:53:44 +0100

    Define store mini blog index controller

---
+++ Controller/Index/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Biglidio\MiniBlog\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+    }
+}
